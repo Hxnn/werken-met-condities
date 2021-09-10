ec2e836a865c6c449b23fb5b8fe1a2be4959966b
Hinn <64025546+TheHinn@users.noreply.github.com>
2021-09-10 13:07:49 +0200

    Mix en Max

--- meten-is-weten.py
+++ meten-is-weten.py
@@ -1,8 +1,12 @@
 a = input('Vul een gehele getal in: ')
 b = input('Vul een gehele getal in: ')
 max(a, b)
+min(b, a)
 if a > b:
       print('A is het grootste getal!' , a)
 elif a < b:
     print('B is het grootste getal!' , b)
-min(b, a)
+elif a == b:
+    print('A en B zijn even groot!')
+if a > b: print("Het maximum =", a) and print ("Het minimum =", b)
+if a < b: print('Het maximum =', b) and print ("Het minimum =", a)
