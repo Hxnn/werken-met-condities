636d552df7bd9a169ccd795548192d24576c8c02
Hinn <64025546+TheHinn@users.noreply.github.com>
2021-09-15 12:45:53 +0200

    Edited code

--- KaasQuiz.py
+++ KaasQuiz.py
@@ -5,19 +5,27 @@ if Kaas == "y":
     Gaten = input('Zitten er gaten in? (y/n) ')
     if Gaten == "y":
         Duur = input('Is de kaas belachelijk duur? (y/n) ')
-        if Duur == "y": print('Emmenthaler')
-        else: print('Leerdammer')
+        if Duur == "y":
+            print('Emmenthaler')
+        else:
+            print('Leerdammer')
     elif Gaten == "n":
         Hard = input('Is de kaas hard als een steen? (y/n) ')
-        if Hard == "y": print('Pammigiano Reggiano')
-        else: print('Goudse Kaas')
+        if Hard == "y":
+            print('Pammigiano Reggiano')
+        else:
+            print('Goudse Kaas')
 if Kaas == "n":
     Schimmels = input('Heeft de kaas blauwe schimmels? (y/n) ')
     if Schimmels == "y":
         Korst1 = input('Heeft de kaas een korst? (y/n) ')
-        if Korst1 == "y": print('Bleu de Rochbaron')
-        else: print("Foume d'Ambert")
+        if Korst1 == "y":
+            print('Bleu de Rochbaron')
+        else:
+            print("Foume d'Ambert")
     elif Schimmels == "n":
         Korst2 = input('Heeft de kaas een korst? (y/n) ')
-        if Korst2 == "y": print('Camembert')
-        else: print('Mozzarella')
+        if Korst2 == "y":
+            print('Camembert')
+        else:
+            print('Mozzarella')
--- meten-is-weten.py
+++ meten-is-weten.py
@@ -3,10 +3,14 @@ b = input('Vul een gehele getal in: ')
 max(a, b)
 min(b, a)
 if a > b:
-      print('A is het grootste getal!' , a)
+      print('A is het grootste getal!', a)
 elif a < b:
-    print('B is het grootste getal!' , b)
+    print('B is het grootste getal!', b)
 elif a == b:
     print('A en B zijn even groot!')
-if a > b: print("Het maximum =", a) and print ("Het minimum =", b)
-if a < b: print('Het maximum =', b) and print ("Het minimum =", a)
+if a > b:
+    print("Het maximum =", a)
+    print("Het minimum =", b)
+if a < b:
+    print('Het maximum =', b)
+    print("Het minimum =", a)
